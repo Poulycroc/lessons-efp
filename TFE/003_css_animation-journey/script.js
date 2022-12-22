fc856b6bf2cd5f0c347304d234c8a99c1f4338e6
Maxime Bartier <poulycroc.studio@gmail.com>
2022-12-22 17:49:30 +0100

    xip

---
+++ TFE/003_css_animation-journey/script.js
@@ -0,0 +1,6 @@
+const sidebarBtnToggle = document.getElementById("sidebarBtnToggle");
+// ajouter le script pour ajouter une class "open" pour la sidebar
+
+
+const modalBtn = document.getElementById("modalBtn");
+// ajouter le script pour ajouter une class "open" pour la modal
